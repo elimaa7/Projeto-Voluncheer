9c854c5ea302585d1b4c5fac5399c5812ae59576
elimaa7 <enricolima06@gmail.com>
2026-06-08 16:31:39 -0300

    corrige imports do server.js

--- Voluncheer-api/server.js
+++ Voluncheer-api/server.js
@@ -1,21 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import basicRoutes from './voluncheer-api/basicRoutes.js'
-import userRoutes from './voluncheer-api/userRoutes.js'
-import eventosRoutes from './voluncheer-api/eventosRoutes.js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname  = path.dirname(__filename)
+import basicRoutes from './basicRoutes.js'
+import userRoutes from './userRoutes.js'
+import eventosRoutes from './eventosRoutes.js'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-// Serve todos os arquivos do frontend em localhost:3000
-app.use(express.static(path.join(__dirname, '/Projeto-Voluncheer')))
 
 const PORT = process.env.PORT || 3000
 
@@ -25,7 +17,4 @@ app.use(eventosRoutes)
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)
-    console.log(`Site: http://localhost:${PORT}`)
-    console.log(`API:  http://localhost:${PORT}/eventos`)
 })
-
